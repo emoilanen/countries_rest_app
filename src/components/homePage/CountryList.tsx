1be05fb9507c1c676b60472b55b3d325bf8787e9
Elisa Moilanen <moieli@hotmail.fi>
2023-02-11 12:45:37 +0200

    fetching data to table

--- src/components/homePage/CountryList.tsx
+++ src/components/homePage/CountryList.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,83 +9,113 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { ChangeEvent, useState } from 'react';
+import { type ChangeEvent, useState, useEffect, useCallback } from 'react';
 import Pagination from './Pagination';
+import axios from 'axios';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { IconButton } from '@mui/material';
 
-
-
-const rows = [ {
-	flag: 'https://lipunkuva.jpg',
-	name: {
-		common: 'Finland',
-		official: 'Finland',
-	},
-	region: 'Europe',
-	population: 55556464,
-	languages: {
-		fi: 'Finnish',
-		swe: 'Swedish'
-	}
-}
+const rows = [
+  {
+    flag: 'https://lipunkuva.jpg',
+    name: {
+      common: 'Finland',
+      official: 'Finland'
+    },
+    region: 'Europe',
+    population: 55556464,
+    languages: {
+      fi: 'Finnish',
+      swe: 'Swedish'
+    }
+  }
 ];
-const CountryList = () => {
 
-	 const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+export interface CountryData {
+  flag?: string;
+  name?: {
+    common: string;
+    official: string;
+  };
+  region?: string;
+  population?: number;
+  languages?: string[];
+}
 
-		const handleChangePage = (event: any, newPage: number): void => {
-		 setPage(newPage);
-	}
+const CountryList = (): JSX.Element => {
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+  const [countryData, setCountryData] = useState<CountryData[]>([]);
 
-	function handleChangeRowsPerPage(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void {
-		 setRowsPerPage(parseInt(event.target.value, 10));
+  useEffect(() => {
+    (async () => {
+      await fetchCountries();
+    })();
+  }, []);
+
+  const fetchCountries = useCallback(async () => {
+    try {
+      await axios.get('https://restcountries.com/v3.1/all').then((response) => {
+        console.log(response.data);
+        setCountryData(response.data);
+      });
+    } catch (e) {
+      console.error('Error while fetching countries', e);
+    }
+  }, []);
+
+  const handleChangePage = useCallback((event: any, newPage: number): void => {
+    setPage(newPage);
+  }, []);
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
-	}
+  };
 
-
-	return (
-			<>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Flag</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Regions</TableCell>
-            <TableCell align="right">Population</TableCell>
-						<TableCell align="right">Languages</TableCell>
-						<TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-						{ rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-							.map((row) => (
-            <TableRow
-              key={row.name.common}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.flag}
-              </TableCell>
-              	<TableCell align="right">{row.name.common}</TableCell>
-              	<TableCell align="right">{row.region}</TableCell>
-              	<TableCell align="right">{row.population}</TableCell>
-								<TableCell align="right">{ row.languages.fi }</TableCell>
-								<TableCell align="right">ArrowButton</TableCell>
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Flag</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Regions</TableCell>
+              <TableCell align="right">Population</TableCell>
+              <TableCell align="right">Languages</TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
-          ))}
-					</TableBody>
-					<Pagination
-						page={ page }
-						rows={ rows }
-						handleChangePage={ handleChangePage }
-						handleChangeRowsPerPage={ handleChangeRowsPerPage }
-						rowsPerPage={rowsPerPage}/>
-			</Table>
-		</TableContainer>
-
-
-	</>
+          </TableHead>
+          <TableBody>
+            {countryData.length > 0 &&
+              countryData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                <TableRow key={row?.name?.common} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {row?.flag}
+                  </TableCell>
+                  <TableCell align="right">{row?.name?.common}</TableCell>
+                  <TableCell align="right">{row?.region}</TableCell>
+                  <TableCell align="right">{row?.population}</TableCell>
+                  {/**     <TableCell align="right">{row?.languages?.fi}</TableCell> */}
+                  <TableCell align="right">
+                    <IconButton>
+                      <ArrowForwardIosIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+          <Pagination
+            page={page}
+            rows={rows}
+            handleChangePage={handleChangePage}
+            handleChangeRowsPerPage={handleChangeRowsPerPage}
+            rowsPerPage={rowsPerPage}
+          />
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
